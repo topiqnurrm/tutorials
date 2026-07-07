3527b709a0c4db310a71f3052657f81b3126739a
Taufiq Nurrohman <sossmeed@gmail.com>
2026-07-07 12:16:06 +0700

    hari 4, lesson 3

---
+++ 1_estate/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "estate",
+    'version': '0.1.0',
+    'depends': ['base'],
+    'author': "topiqnurrm",
+    'category': 'Tutorials',
+    'description': """
+    ini adalah modul estate untuk tutorial odoo 19
+    """,
+    # data files always loaded at installation
+    'data': [
+        # 'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        # 'demo/demo_data.xml',
+    ],
+
+    "application": True,
+    "auto_install": False,
+}
---
+++ 1_estate/models/estate_property.py
@@ -0,0 +1,7 @@
+from odoo import models, fields
+
+class EstateProperties(models.Model):
+    _name = "estate.properties"
+    _description = "estate properties ya ini deskripsi"
+
+    
